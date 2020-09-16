cea3fef087f86baeab67461a56a267236b96d238
tsutterley <tsutterl@uw.edu>
2020-09-15 17:05:33 -0700

    update symlink for full path
    add verbose options for pytest

--- data/test_font_files.py
+++ data/test_font_files.py
@@ -8,13 +8,16 @@ import warnings
 import matplotlib.font_manager
 import matplotlib.pyplot as plt
 
-def test_font_files():
-    basedir = os.path.join(os.sep,'usr','share','fonts','truetype','Helvetica')
+def test_font_files(verbose=True):
+    basedir = os.path.join(os.sep,'usr','share','fonts','truetype')
     fonts = ['HelveticaBoldOblique.ttf','HelveticaBold.ttf',
         'HelveticaLightOblique.ttf','HelveticaLight.ttf',
         'HelveticaOblique.ttf','Helvetica.ttf']
+    print(os.listdir(basedir)) if verbose else None
+    assert os.path.isdir(os.path.join(basedir,'Helvetica'))
+    print(os.listdir(os.path.join(basedir,'Helvetica'))) if verbose else None
     for fpath in fonts:
-        assert os.path.exists(os.path.join(basedir, fpath))
+        assert os.path.exists(os.path.join(basedir,'Helvetica',fpath))
     #-- rebuilt the matplotlib fonts and set parameters
     matplotlib.font_manager._rebuild()
     matplotlib.rcParams['axes.linewidth'] = 1.5
