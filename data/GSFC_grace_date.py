e95421f40c16335b6e7b9e283027d14e08ca1f44
tsutterley <tsutterl@uw.edu>
2023-06-07 17:20:53 -0700

    update GSFC grace date to use pathlib

--- data/GSFC_grace_date.py
+++ data/GSFC_grace_date.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 u"""
 GSFC_grace_date.py
-Written by Tyler Sutterley (04/2023)
+Written by Tyler Sutterley (05/2023)
 
 Reads dates of GSFC GRACE mascon data file and assigns the month number
     reads the start and end date from the filename,
@@ -27,6 +27,7 @@ PROGRAM DEPENDENCIES:
     utilities.py: download and management utilities for syncing files
 
 UPDATE HISTORY:
+    Updated 05/2023: use pathlib to define and operate on paths
     Updated 04/2023: added newer GSFC mascons for RL06v2.0
     Updated 01/2023: single implicit import of gravity toolkit
     Updated 10/2022: added links to newer GSFC mascons Release-6 Version 2.0
@@ -46,16 +47,20 @@ import sys
 import os
 import h5py
 import inspect
+import logging
+import pathlib
 import requests
 import argparse
 import posixpath
 import numpy as np
 import gravity_toolkit as gravtk
 
-#-- PURPOSE: get GSFC GRACE mascon data
+# PURPOSE: get GSFC GRACE mascon data
 def get_GSFC_grace_mascons(base_dir, TIMEOUT=None, RETRY=5,
     VERSION=None, MODE=0o775):
-    #-- remote path for mascon versions
+    # set GRACE parameters
+    PROC, DSET = 'GSFC', 'GSM'
+    # remote path for mascon versions
     HOST = {}
     HOST['v02.4'] = ['https://earth.gsfc.nasa.gov','sites','default','files',
         'neptune','grace','mascons_2.4','GSFC.glb.200301_201607_v02.4.hdf']
@@ -71,26 +76,30 @@ def get_GSFC_grace_mascons(base_dir, TIMEOUT=None, RETRY=5,
     #     '2022-10','gsfc.glb_.200204_202207_rl06v2.0_obp-ice6gd.h5']
     HOST['rl06v2.0'] = ['https://earth.gsfc.nasa.gov','sites','default','files',
         '2023-03','gsfc.glb_.200204_202211_rl06v2.0_obp-ice6gd.h5']
-    #-- local file
-    local = os.path.join(base_dir,'GSFC',VERSION,'GSM',HOST[VERSION][-1])
-    #-- attempt to download up to the number of retries
+    # local file
+    base_dir = pathlib.Path(base_dir).expanduser().absolute()
+    local = base_dir.joinpath(PROC,VERSION,DSET,HOST[VERSION][-1])
+    # check if the local file exists
+    if local.exists():
+        return
+    # attempt to download up to the number of retries
     retry_counter = 0
     while (retry_counter < RETRY):
         try:
-            #-- get GSFC GRACE mascon file
+            # get GSFC GRACE mascon file
             from_http(HOST[VERSION], timeout=TIMEOUT, local=local,
                 verbose=True, mode=MODE)
         except:
             pass
         else:
             return
-        #-- add to retry counter
+        # add to retry counter
         retry_counter += 1
-    #-- check if maximum number of retries were reached
+    # check if maximum number of retries were reached
     if (retry_counter == RETRY):
         raise TimeoutError('Maximum number of retries reached')
 
-#-- PURPOSE: download a file from a http host
+# PURPOSE: download a file from a http host
 def from_http(HOST,timeout=None,local=None,verbose=False,mode=0o775):
     """
     Download a file from a http host
@@ -106,30 +115,32 @@ def from_http(HOST,timeout=None,local=None,verbose=False,mode=0o775):
     verbose: print file transfer information
     mode: permissions mode of output local file
     """
-    #-- get GSFC GRACE mascon file
+    # get GSFC GRACE mascon file
     req = requests.get(posixpath.join(*HOST), timeout=timeout,
         allow_redirects=True)
-    #-- get last modified time of GRACE mascon file
+    # get last modified time of GRACE mascon file
     last_modified = req.headers['last-modified']
     mtime = gravtk.utilities.get_unix_time(last_modified,
         format='%a, %d %b %Y %H:%M:%S %Z')
-    #-- recursively create local directory if non-existent
-    if not os.access(os.path.dirname(local),os.F_OK):
-        os.makedirs(os.path.dirname(local),mode)
-    #-- print file information
+    # recursively create local directory if non-existent
+    local.parent.mkdir(mode=mode, parents=True, exist_ok=True)
+    # print file information
     args = (posixpath.join(*HOST),local)
     print('{0} -->\n\t{1}'.format(*args)) if verbose else None
-    with open(local, 'wb') as f:
+    with local.open(mode='wb') as f:
         f.write(req.content)
-    #-- keep remote modification time of file and local access time
-    os.utime(local, (os.stat(local).st_atime, mtime))
-    #-- change the permissions mode
-    os.chmod(local, mode)
+    # keep remote modification time of file and local access time
+    os.utime(local, (local.stat().st_atime, mtime))
+    # change the permissions mode
+    local.chmod(mode=mode)
 
 def GSFC_grace_date(base_dir, VERSION='v02.4', MODE=0o775):
-    #-- set the GRACE directory
-    grace_dir = os.path.join(base_dir,'GSFC',VERSION,'GSM')
-    #-- dictionary of GSFC GRACE mascon files
+    # set GRACE parameters
+    PROC, DSET = 'GSFC', 'GSM'
+    # set the GRACE directory
+    base_dir = pathlib.Path(base_dir).expanduser().absolute()
+    grace_dir = base_dir.joinpath(PROC,VERSION,DSET)
+    # dictionary of GSFC GRACE mascon files
     grace_file = {}
     grace_file['v02.4'] = 'GSFC.glb.200301_201607_v02.4.hdf'
     # grace_file['rl06v1.0'] = 'gsfc.glb_.200204_202009_rl06v1.0_obp-ice6gd.h5'
@@ -138,142 +149,154 @@ def GSFC_grace_date(base_dir, VERSION='v02.4', MODE=0o775):
     # grace_file['rl06v2.0'] = 'gsfc.glb_.200204_202112_rl06v2.0_obp-ice6gd.h5'
     # grace_file['rl06v2.0'] = 'gsfc.glb_.200204_202207_rl06v2.0_obp-ice6gd.h5'
     grace_file['rl06v2.0'] = 'gsfc.glb_.200204_202211_rl06v2.0_obp-ice6gd.h5'
-    #-- valid date string (HDF5 attribute: 'days since 2002-01-00T00:00:00')
+    # valid date string (HDF5 attribute: 'days since 2002-01-00T00:00:00')
     date_string = 'days since 2002-01-01T00:00:00'
     epoch,to_secs = gravtk.time.parse_date_string(date_string)
-    #-- dictionary of start, end and mid-dates as Modified Julian Days
+    # dictionary of start, end and mid-dates as Modified Julian Days
     MJD = {}
-    #-- read the HDF5 file
-    with h5py.File(os.path.join(grace_dir,grace_file[VERSION]),'r') as fileID:
+    # read the HDF5 file
+    input_file = grace_dir.joinpath(grace_file[VERSION])
+    with h5py.File(input_file, mode='r') as fileID:
+        # output HDF5 file information
+        logging.info(fileID.filename)
+        logging.info(list(fileID.keys()))
+        # get GSFC mascon dates
         nmas,nt = fileID['solution']['cmwe'].shape
-        #-- convert from reference days to Modified Julian Days
+        # convert from reference days to Modified Julian Days
         for key in ['ref_days_first','ref_days_last','ref_days_middle']:
             MJD[key] = gravtk.time.convert_delta_time(
                 to_secs*fileID['time'][key][:].flatten(),
                 epoch1=epoch, epoch2=(1858,11,17,0,0,0), scale=1.0/86400.0)
 
-    #-- convert from Modified Julian Days to calendar days
+    # convert from Modified Julian Days to calendar days
     start_yr,M1,D1,h1,m1,s1 = gravtk.time.convert_julian(2400000.5 +
         MJD['ref_days_first'], FORMAT='tuple')
     end_yr,M2,D2,h2,m2,s2 = gravtk.time.convert_julian(2400000.5 +
         MJD['ref_days_last'], FORMAT='tuple')
     YY,MM,DD,hh,mm,ss = gravtk.time.convert_julian(2400000.5 +
         MJD['ref_days_middle'], FORMAT='tuple')
-    #-- convert mid-date calendar dates to year-decimal
+    # convert mid-date calendar dates to year-decimal
     tdec = gravtk.time.convert_calendar_decimal(YY,MM,day=DD,
         hour=hh,minute=mm,second=ss)
 
-    #-- create day of year variables
+    # create day of year variables
     start_day = np.zeros((nt))
     end_day = np.zeros((nt))
-    #-- days per month in a leap and a standard year
-    #-- only difference is February (29 vs. 28)
+    # days per month in a leap and a standard year
+    # only difference is February (29 vs. 28)
     dpm_leap = np.array([31,29,31,30,31,30,31,31,30,31,30,31], dtype=np.float64)
     dpm_stnd = np.array([31,28,31,30,31,30,31,31,30,31,30,31], dtype=np.float64)
-    #-- create matrix with the lower half = 1
+    # create matrix with the lower half = 1
     mon_mat = np.tri(12,12,-1)
-    #-- find indices for standard years and leap years
+    # find indices for standard years and leap years
     lp1, = np.nonzero(((start_yr % 4) == 0))
     sd1, = np.nonzero(((start_yr % 4) != 0))
     lp2, = np.nonzero(((end_yr % 4) == 0))
     sd2, = np.nonzero(((end_yr % 4) != 0))
-    #-- convert from months to months indices
+    # convert from months to months indices
     m1_m1 = np.array(M1, dtype=np.int64) - 1
     m2_m1 = np.array(M2, dtype=np.int64) - 1
-    #-- calculate the day of the year for leap and standard
-    #-- use total days of all months before date
-    #-- and add number of days before date in month
+    # calculate the day of the year for leap and standard
+    # use total days of all months before date
+    # and add number of days before date in month
     start_day[sd1] = (D1[sd1]-1) + np.dot(mon_mat[m1_m1[sd1],:],dpm_stnd)
     start_day[lp1] = (D1[lp1]-1) + np.dot(mon_mat[m1_m1[lp1],:],dpm_leap)
     end_day[sd2] = (D2[sd2]-1) + np.dot(mon_mat[m2_m1[sd2],:],dpm_stnd)
     end_day[lp2] = (D2[lp2]-1) + np.dot(mon_mat[m2_m1[lp2],:],dpm_leap)
 
-    #-- calculate the GRACE month (Apr02 == 004)
-    #-- https://grace.jpl.nasa.gov/data/grace-months/
-    #-- Notes on special months (e.g. 119, 120) below
+    # calculate the GRACE month (Apr02 == 004)
+    # https://grace.jpl.nasa.gov/data/grace-months/
+    # Notes on special months (e.g. 119, 120) below
     grace_month = np.array(12*(YY - 2002) + MM, dtype=np.int64)
-    #-- calculating the month number of 'Special Months' with accelerometer
-    #-- shutoffs is more complicated as days from other months are used
+    # calculating the month number of 'Special Months' with accelerometer
+    # shutoffs is more complicated as days from other months are used
     grace_month = gravtk.time.adjust_months(grace_month)
 
-    #-- Output GRACE date ascii file
-    grace_date_file = '{0}_{1}_DATES.txt'.format('GSFC', VERSION)
-    fid = open(os.path.join(grace_dir,grace_date_file), 'w')
-    #-- date file header information
+    # Output GRACE date ascii file
+    grace_date_file = grace_dir.joinpath(f'{PROC}_{VERSION}_DATES.txt')
+    fid = grace_date_file.open(mode='w', encoding='utf-8')
+    # date file header information
     args = ('Mid-date','Month','Start_Day','End_Day','Total_Days')
     print('{0} {1:>10} {2:>11} {3:>10} {4:>13}'.format(*args),file=fid)
 
-    #-- calculate total number of days since Jan 2002
+    # calculate total number of days since Jan 2002
     tot_days = np.zeros((nt))
 
-    #-- for each date
+    # for each date
     for t,mon in enumerate(grace_month):
-        #-- number of days in the year
+        # number of days in the year
         dpy = gravtk.time.calendar_days(start_yr[t]).sum()
-        #-- For data that crosses years
+        # For data that crosses years
         end_cyclic = (end_yr[t] - start_yr[t])*dpy + end_day[t]
-        #-- Calculation of total days since start of campaign
+        # Calculation of total days since start of campaign
         count = 0
         n_yrs = np.int64(start_yr[t]-2002)
-        #-- for each of the GRACE years up to the file year
+        # for each of the GRACE years up to the file year
         for iyr in range(n_yrs):
-            #-- year i
+            # year i
             year = 2002 + iyr
-            #-- number of days in year i (if leap year or standard year)
-            #-- add all days from prior years to count
+            # number of days in year i (if leap year or standard year)
+            # add all days from prior years to count
             count += gravtk.time.calendar_days(year).sum()
 
-        #-- calculating the total number of days since 2002
+        # calculating the total number of days since 2002
         tot_days[t] = np.mean([count+start_day[t], count+end_cyclic])
 
-        #-- print to GRACE DATES ascii file (NOTE: tot_days will be rounded up)
+        # print to GRACE DATES ascii file (NOTE: tot_days will be rounded up)
         print(('{0:13.8f} {1:03d} {2:8.0f} {3:03.0f} {4:8.0f} {5:03.0f} '
             '{6:8.0f}').format(tdec[t],mon,start_yr[t],start_day[t],
             end_yr[t],end_day[t],tot_days[t]), file=fid)
 
-    #-- close date file
-    #-- set permissions level of output date file
+    # close date file
+    # set permissions level of output date file
     fid.close()
-    os.chmod(os.path.join(grace_dir, grace_date_file), MODE)
+    grace_date_file.chmod(mode=MODE)
 
-#-- This is the main program that calls the individual modules
+# This is the main program that calls the individual modules
 def main():
-    #-- Read the system arguments listed after the program
+    # Read the system arguments listed after the program
     parser = argparse.ArgumentParser(
         description="""Reads dates of GSFC GRACE mascon data files and assigns
             the month number
             """
     )
-    #-- current file path
+    # current file path
     filename = inspect.getframeinfo(inspect.currentframe()).filename
-    filepath = os.path.dirname(os.path.abspath(filename))
-    #-- working data directory
+    filepath = pathlib.Path(filename).absolute().parent
+    # working data directory
     parser.add_argument('--directory','-D',
-        type=lambda p: os.path.abspath(os.path.expanduser(p)), default=filepath,
+        type=pathlib.Path, default=filepath,
         help='Working data directory')
-    #-- GSFC GRACE mascon version
+    # GSFC GRACE mascon version
     parser.add_argument('--version','-v',
         type=str, default='rl06v2.0',
         help='GSFC GRACE mascon version')
-    #-- connection timeout and number of retry attempts
+    # connection timeout and number of retry attempts
     parser.add_argument('--timeout','-t',
         type=int, default=360,
         help='Timeout in seconds for blocking operations')
     parser.add_argument('--retry','-r',
         type=int, default=5,
         help='Connection retry attempts')
-    #-- permissions mode of the local files (number in octal)
+    parser.add_argument('--verbose','-V',
+        action='count', default=0,
+        help='Verbose output of processing run')
+    # permissions mode of the local files (number in octal)
     parser.add_argument('--mode','-M',
         type=lambda x: int(x,base=8), default=0o775,
         help='Permission mode of directories and files synced')
     args = parser.parse_args()
 
-    #-- get GSFC GRACE mascon data
+    # create logger
+    loglevels = [logging.CRITICAL, logging.INFO, logging.DEBUG]
+    logging.basicConfig(level=loglevels[args.verbose])
+
+    # get GSFC GRACE mascon data
     get_GSFC_grace_mascons(args.directory, TIMEOUT=args.timeout,
         RETRY=args.retry, VERSION=args.version, MODE=args.mode)
-    #-- run GSFC GRACE mascon date program
+    # run GSFC GRACE mascon date program
     GSFC_grace_date(args.directory, VERSION=args.version, MODE=args.mode)
 
-#-- run main program
+# run main program
 if __name__ == '__main__':
     main()
